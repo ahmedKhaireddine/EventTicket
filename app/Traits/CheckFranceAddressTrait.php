da5aab27bdac8068438ffb0aef36fcdb406c477f
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2020-03-23 19:08:52 +0100

    Add CheckFranceAddressTrait to validate French addresses

---
+++ app/Traits/CheckFranceAddressTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits;
+
+trait CheckFranceAddressTrait
+{
+    public function checkAddress(array $attributes)
+    {
+        if (isset($attributes['street_address'], $attributes['postal_code'])) {
+            $postCode = $attributes['postal_code'];
+            $q = urlencode($attributes['street_address']);
+
+            $dataAddress = json_decode(file_get_contents("https://api-adresse.data.gouv.fr/search/?limit=1&postCode={$postCode}&q={$q}"), true);
+
+            $venueAddress = $dataAddress['features'][0]['properties'];
+
+            if (ucwords($venueAddress['name']) === ucwords($attributes['street_address']) &&
+                ucwords($venueAddress['postcode']) === ucwords($attributes['postal_code']) &&
+                ucwords($venueAddress['city']) === ucwords($attributes['city'])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
