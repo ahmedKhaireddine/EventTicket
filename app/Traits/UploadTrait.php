ad6271baba42ed1d4cc05a0ceb1ddc38e462730a
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2020-05-01 17:36:31 +0200

    Improve UploadTrait

--- app/Traits/UploadTrait.php
+++ app/Traits/UploadTrait.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Http\UploadeFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 trait UploadTrait
 {
@@ -13,12 +14,12 @@ trait UploadTrait
      * @param  object  $uploadeFile
      * @param  string  $folder
      * @param  string  $disk
-     * @param  string  $filename
+     * @param  int  $id
      * @return string
      */
-    public function uploadOne($uploadeFile, string $folder, string $disk = 'public', string $filename = null)
+    public function uploadOne($uploadeFile, string $folder, string $disk = 'public', int $id)
     {
-        $name = isset($filename) ? str_replace(' ', '_', $filename).'_'.time() : str_random(25).'_'.time();
+        $name = 'event_'. $id . '_' . time();
 
         $filePath = "http://localhost/api/public{$folder}{$name}.{$uploadeFile->getClientOriginalExtension()}";
 
